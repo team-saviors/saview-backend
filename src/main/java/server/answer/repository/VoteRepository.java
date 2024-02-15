247571bff6c5f4ab4b45eeb475afc6e4f449b32f
klkim1913 <krkim1913@gmail.com>
2024-02-15 16:12:20 +0900

    Chore: "재적용 .gitignore file"
    bulid 폴더 내 파일 트래킹으로 인한 rm --cached

--- src/main/java/server/answer/repository/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package server.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.answer.entity.Vote;
-
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-    boolean existsByAnswerIdAndUserId(long answerId, long userId);
-}
--- src/main/java/server/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.chat.model.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
